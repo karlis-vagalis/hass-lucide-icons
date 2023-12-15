4a3003aa217518a0a5cde8ce532f555cc59f4099
VAGALIS, Karlis <karlis.vagalis@gmail.com>
2023-12-15 13:56:49 +0100

    Removed unneccessary code. Possible solution to Android WebView not working issue. Still open

--- custom_components/lucideicons/data/lucide.js
+++ custom_components/lucideicons/data/lucide.js
@@ -68,7 +68,9 @@ customElements.whenDefined("ha-icon").then(() => {
     const HaIcon = customElements.get("ha-icon");
 
     HaIcon.prototype._setCustomPath = async function (promise, requestedIcon) {
+
         const icon = await promise;
+
         if (requestedIcon !== this.icon || icon === undefined) return;
         this._path = icon.path;
         this._viewBox = icon.viewBox;
@@ -76,99 +78,11 @@ customElements.whenDefined("ha-icon").then(() => {
         await this.UpdateComplete;
 
         const el = this.shadowRoot.querySelector("ha-svg-icon");
-        if (!el || !el.setPaths) {
-            return;
-        }
-        el.clearPaths();
+        const root = el.shadowRoot;
 
-        if (icon.fullCode) {
+        //console.log(root);
 
-            console.log("HI BIOZTC")
+        root.innerHTML = icon.fullCode;
 
-            await el.updateComplete;
-            const root = el.shadowRoot.querySelector("svg");
-
-            root.outerHTML = icon.fullCode;
-
-            /*
-            const styleEl = document.createElement("style");
-            styleEl.innerHTML = `
-          svg:first-child>g:first-of-type>path {
-            display: none;
-          }
-        `;
-            root.appendChild(styleEl);
-            root.appendChild(icon.fullCode.cloneNode(true));
-            */
-        } else {
-            el.setPaths(icon.paths);
-            if (icon.format) {
-                el.classList.add(...icon.format.split("-"));
-            }
-        }
-    };
-});
-
-customElements.whenDefined("ha-svg-icon").then(() => {
-    const HaSvgIcon = customElements.get("ha-svg-icon");
-
-    HaSvgIcon.prototype.clearPaths = async function () {
-        await this.updateComplete;
-
-        const svgRoot = this.shadowRoot.querySelector("svg");
-        while (svgRoot && svgRoot.children.length > 1)
-            svgRoot.removeChild(svgRoot.lastChild);
-
-        const svgGroup = this.shadowRoot.querySelector("g");
-        while (svgGroup && svgGroup.children.length > 1)
-            svgGroup.removeChild(svgGroup.lastChild);
-
-        while (this.shadowRoot.querySelector("style.fontawesome")) {
-            const el = this.shadowRoot.querySelector("style.fontawesome");
-            el.parentNode.removeChild(el);
-        }
-    };
-
-    HaSvgIcon.prototype.setPaths = async function (paths) {
-        await this.updateComplete;
-        if (paths == undefined || Object.keys(paths).length === 0) return;
-        const styleEl =
-            this.shadowRoot.querySelector("style.fontawesome") ||
-            document.createElement("style");
-        styleEl.classList.add("fontawesome");
-        styleEl.innerHTML = `
-        .secondary {
-          opacity: 0.4;
-        }
-        :host(.invert) .secondary {
-          opacity: 1;
-        }
-        :host(.invert) .primary {
-          opacity: 0.4;
-        }
-        :host(.color) .primary {
-          opacity: 1;
-        }
-        :host(.color) .secondary {
-          opacity: 1;
-        }
-        :host(.color:not(.invert)) .secondary {
-          fill: var(--icon-secondary-color, var(--disabled-text-color));
-        }
-        :host(.color.invert) .primary {
-          fill: var(--icon-secondary-color, var(--disabled-text-color));
-        }
-        path:not(.primary):not(.secondary) {
-          opacity: 0;
-        }
-        `;
-        this.shadowRoot.appendChild(styleEl);
-        const root = this.shadowRoot.querySelector("g");
-        for (const k in paths) {
-            const el = document.createElementNS("http://www.w3.org/2000/svg", "path");
-            el.setAttribute("d", paths[k]);
-            el.classList.add(k);
-            root.appendChild(el);
-        }
     };
 });
